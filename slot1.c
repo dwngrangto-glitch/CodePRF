c76b7633bdb380b6ce8ce944e9003db010a9b6a6
Nguyễn Hoàng Hải Đăng <dwngrangto@gmail.com>
2026-01-09 10:17:48 +0700

    Delete slot1.c

--- slot1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-	int tuoi = 20;
-	char hoten[] = "Nguyen Hoang Hai Dang";
-	printf("Ten t la: %s\n", hoten);    //%s là khai báo kiểu kí tự dạng chuỗi, %c là khai báo char
-	printf("Tuoi t la: %d", tuoi);     //%d là khai báo kiểu số nguyên
-	return 0;
-}
