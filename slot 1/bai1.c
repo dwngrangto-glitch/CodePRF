0e5cb480790c987e6594366d7491470b8b5c6e15
Your Name <you@example.com>
2026-01-09 10:10:49 +0700

    Add slot 1 - bai 1

---
+++ slot 1/bai1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	int tuoi = 20;
+	char hoten[] = "Nguyen Hoang Hai Dang";
+	printf("Ten t la: %s\n", hoten);    //%s là khai báo kiểu kí tự//
+	printf("Tuoi t la: %d", tuoi);     //%d là khai báo kiểu số nguyên//
+	return 0;
+}
